da8835a2ad5f7f557dd270b2f6dd55a3ae44f5b7
Georges TAVERNE <gtaverne@e3r5p15.42.fr>
2021-10-29 16:10:57 +0200

    On reprend tout et on fait un rbt

--- sources/tree.hpp
+++ sources/tree.hpp
@@ -130,44 +130,132 @@ private:
 // Tree Utils                                               //
 //**********************************************************//
 
-Node *_new_node(value_type cont &rawdata)
-{
-	Node *node = _alloc.allocate(1);
-	_alloc.construct(node, rawdata);
-	node->color = RED;
-	node->left = NIL;
-	node->right = NIL;
-	node->parent = NIL;
+	Node *_new_node(value_type const &rawdata)
+	{
+		Node *node = _alloc.allocate(1);
+		_alloc.construct(node, rawdata);
+		node->color = RED;
+		node->left = NIL;
+		node->right = NIL;
+		node->parent = NIL;
 
-	return node;
-}
+		return node;
+	}
 
-Node *_papynode(Node const *node) const
-{
-	return node->parent->parent;
-}
-
-Node *_brothernode(Node const *node) const
-{
-	if (node == node->parent->left)
-		return (node->parent->right);
-	else
-		return (node->parent>left);
-}
-
-Node *_unclenode(Node const *node) const
-{
-	
-}
+/*
+Left Rotation
+     X                  Y
+	/ \                / \
+   a   Y       =>     X   c
+      / \            / \  
+	 b   c          a   b
+*/
 
 
-//**********************************************************//
-// RBT algo functions                                       //
-//**********************************************************//
+	void _left_rotate(Node *x)
+	{
+		Node *y = x->right;
+		x->right = y->left;
+
+		if (y->left != NIL)
+			y->left->parent = x;
+		y->parent = x->parent;
+		if (x->parent == NIL)
+			_root = y;
+		else if (x == x->parent->left)
+			x->parent->left = y;
+		else
+			x->parent->right = y
+		y->left = x;
+		x->parent = y;
+	}
+
+/*
+Right Rotation
+     X                  Y
+	/ \                / \
+   Y   c       =>     a   X
+  / \                    / \  
+ a	 b                  b   c
+*/
 
 
+	void _right_rotate(Node *x)
+	{
+		Node *y = x->left;
+		x->left = y->right;
+
+		if (y->right != NIL)
+			y->right->parent = x;
+		y->parent = x->parent;
+		if (x->parent == NIL)
+			_root = y;
+		else if (x == x->parent->right)
+			x->parent->right = y;
+		else
+			x->parent->right = y
+		y->right = x;
+		x->parent = y;
+	}
+
+	void insertValue(value_type const &value)
+	{
+		Node *node = _new_node(value);
+
+		ft::pair<Node *, bool> res = _insert_recursive (root, n);
+		
+	}
 
 
+//Insert a pair in the tree
+	ft::pair<Node *, bool> _insert_recursive(Node *rt, Node *n)
+	{
+		if (root != NIL && _comp(n->val, rt->val))
+		{
+			if (rt->left != NIL)
+				return _insert_recursive(rt->left, n);
+			else
+				rt->left = n;			
+		}
+		else if (rt != NIL && _comp(rt->val, n->val))
+		{
+			if (rt->right != NIL)
+				return _insert_recursive(rt->right, n);
+			else
+				rt->right = n;
+		}
+		else if (rt != NIL)
+			return ft::make_pair(rt, false);
+		n->parent = rt;
+		n->color = RED;
+		n->left = NIL;
+		n->right = NIL;
+		return ft::make_pair(n, true);
+	}
+
+/*
+Feel free to check the detail of RBT algo to understand the next function
+
+*/
+
+
+//Insert fixup
+	void _insertFix (Node *node)
+	{
+		Node *tmp;
+
+		while (k->parent->color == RED)
+		{
+			if (k->parent == k->parent->parent->right)
+			{
+				tmp = k->parent->parent->left; //So uncle
+				if (tmp->color == RED) //Red parent and red uncle :
+				{
+
+				}
+			}
+		}
+	}	
 
 	void _branch_copy(Tree &dest, Node *src, Node *nil_src)
 	{
@@ -178,7 +266,6 @@ Node *_unclenode(Node const *node) const
 			_branch_copy(dest, src->right, nil_src);
 		}
 	}
-	
 
 	void _branch_clear(Node * x = NULL)
 	{
